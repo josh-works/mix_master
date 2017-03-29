1d906253076e02f6d8d2deefe9326ca2ec2c2d14
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-28 21:27:37 -0600

    model and stuff

--- spec/features/user_creates_an_artist_spec.rb
+++ spec/features/user_creates_an_artist_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "User creates an artist" do
     artist_image_path = "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
 
     visit '/artists'
-    click_on "New artist"
+    click_on "New Artist"
     fill_in "artist_name", with: artist_name
     fill_in "artist_image_path", with: artist_image_path
     click_on "Create Artist"
